1910fd85268ff5e7bd7ac0c7a7c61ea84b24dce3
Sakshi Choudhary <122447947+Sakshi2601@users.noreply.github.com>
2025-11-13 22:24:29 +0530

    Add files via upload

---
+++ DSProject.sql
@@ -0,0 +1,19 @@
+﻿
+Microsoft Visual Studio Solution File, Format Version 12.00
+# SQL Server Management Studio Solution File, Format Version 19.00
+VisualStudioVersion = 15.0.28307.421
+MinimumVisualStudioVersion = 10.0.40219.1
+Global
+	GlobalSection(SolutionConfigurationPlatforms) = preSolution
+		Default|Default = Default|Default
+	EndGlobalSection
+	GlobalSection(ProjectConfigurationPlatforms) = postSolution
+		{45483281-A515-45A0-8B55-77116B6BC84F}.Default|Default.ActiveCfg = Default
+	EndGlobalSection
+	GlobalSection(SolutionProperties) = preSolution
+		HideSolutionNode = FALSE
+	EndGlobalSection
+	GlobalSection(ExtensibilityGlobals) = postSolution
+		SolutionGuid = {59B362D2-5F7F-46FE-A2B9-EDC00A8B5291}
+	EndGlobalSection
+EndGlobal
